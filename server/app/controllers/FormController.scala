c4b1913ad74d727ce53bbea08ae05be6126af6df
tempixtl <tom@lauerman.dev>
2021-03-17 22:03:33 -0500

    Refactor form route/file names

--- server/app/controllers/FormController.scala
+++ server/app/controllers/FormController.scala
@@ -8,20 +8,20 @@ import play.api.mvc._
 @Singleton
 class FormController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def getForm = Action { implicit request =>
+  def forms = Action { implicit request =>
     Ok(views.html.forms())
   }
 
   def getFormBasicText(name: String, favColor: String) = Action {
     // Ok(s"$name has the favorite color $favColor.")
-    Ok(views.html.basicText(favColor, name))
+    Ok(views.html.basicTextGet(favColor, name))
   }
 
   def postFormBasicText = Action { request =>
     val postVals = request.body.asFormUrlEncoded
     postVals.map { args =>
       val username = args("username").head
-      Ok(views.html.basicPost(username))
+      Ok(views.html.basicTextPost(username))
     }.getOrElse(Ok("data pls"))
   }
   
